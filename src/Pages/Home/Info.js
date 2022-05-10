3cca964b538d6d09e37f79caa72a7b7417bb7758
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-10 17:08:38 +0600

    infocard completed

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-20'>
+          <InfoCard cardTitle="Opening hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>  
+          <InfoCard cardTitle="Our locations" bgClass="bg-accent" img={marker}></InfoCard>  
+          <InfoCard cardTitle="Contact us" bgClass="bg-gradient-to-r from-primary to-secondary" img={phone}></InfoCard>  
         </div>
     );
 };
