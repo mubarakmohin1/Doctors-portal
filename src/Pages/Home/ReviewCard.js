85a465a6e390e086997fd3884171fb625d90b4a8
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-11 02:16:54 +0600

    review card completed

---
+++ src/Pages/Home/ReviewCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '../Shared/Button';
+
+const ReviewCard = ({ review }) => {
+    const { img,reviewDetails,name } = review;
+    return (
+        <div>
+            <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <p>{reviewDetails}</p>
+                    <img className='w-20 mx-auto mt-4' src={img} alt="" />
+                    <h2 class="card-title mx-auto">{name}</h2>
+                     
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewCard;
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import quote from '../../assets/icons/quote.svg'
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
+import ReviewCard from './ReviewCard';
 
 const Testimonials = () => {
+    const reviews =[
+        {
+            _id:1,
+            name:'William carry',
+            reviewDetails:'Find the lowest price for California Dental Clinic today! Now on sale at GigaPromo. GigaPromo is the website to compare California Dental Clinic. Search and save now! Save Online. The Best Price.',
+            img:people1
+        },
+        {
+            _id:2,
+            name:'John setry',
+            reviewDetails:'Find the lowest price for California Dental Clinic today! Now on sale at GigaPromo. GigaPromo is the website to compare California Dental Clinic. Search and save now! Save Online. The Best Price.',
+            img:people2
+        },
+        {
+            _id:3,
+            name:'Paul xeymen',
+            reviewDetails:'Find the lowest price for California Dental Clinic today! Now on sale at GigaPromo. GigaPromo is the website to compare California Dental Clinic. Search and save now! Save Online. The Best Price.',
+            img:people3
+        },
+    ]
     return (
-        <div className='mt-10'>
-            <h3 className='text-xl text-primary font-bold'>Testimonials</h3>
-            <h2 className='text-3xl'>What are patients say!</h2>
-        </div>
+        <section className='mt-10'>
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className='text-xl text-primary font-bold'>Testimonials</h3>
+                    <h2 className='text-3xl'>What are patients say!</h2>
+                </div>
+                <div>
+                    <img className='w-24 lg:w-48' src={quote} alt="" />
+                </div>
+            </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3'>
+{reviews.map(review =><ReviewCard key={review._id} review ={review}></ReviewCard>)}
+            </div>
+        </section>
     );
 };
 
