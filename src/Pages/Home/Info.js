5b4b8d71ddf355604c72022680d7f65eb5e7b8d7
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-11 03:24:02 +0600

    colorChange

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-          <InfoCard cardTitle="Opening hours" bgclass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>  
-          <InfoCard cardTitle="Our locations" bgclass="bg-neutral" img={marker}></InfoCard>  
-          <InfoCard cardTitle="Contact us" bgclass="bg-gradient-to-r from-primary to-secondary" img={phone}></InfoCard>  
+          <InfoCard cardTitle="Opening hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>  
+          <InfoCard cardTitle="Our locations" bgClass="bg-accent" img={marker}></InfoCard>  
+          <InfoCard cardTitle="Contact us" bgClass="bg-gradient-to-r from-primary to-secondary" img={phone}></InfoCard>  
         </div>
     );
 };
