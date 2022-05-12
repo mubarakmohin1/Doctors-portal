bafcc899e58a0771c60104de603bb7be31c05424
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-12 13:12:07 +0600

    modal connentced

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,12 +1,14 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointment = ({date}) => {
     const [services,setServices] = useState([]);
+    const [treatment,setTreatment] = useState(null);
     useEffect(()=>{
         fetch('service.json').then(res =>res.json()).then(data =>{
-            console.log(data);
+            
             setServices(data)
         })
     },[])
@@ -15,9 +17,10 @@ const AvailableAppointment = ({date}) => {
           <h4 className='text-center text-xl text-secondary'> Available Appointment on {format(date ||new Date(),'PP')}</h4>
           <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
               {
-                  services.map(service =><Service key={service._id} service = {service}></Service>)
+                  services.map(service =><Service key={service._id} service = {service} setTreatment = {setTreatment}></Service>)
               }
           </div>
+          {treatment && <BookingModal treatment={treatment}></BookingModal>}
         </div>
     );
 };
