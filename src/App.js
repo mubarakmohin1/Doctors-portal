1c5f0c22cb34125a14f47d7712ddfcaa7a753921
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-12 08:58:19 +0600

    datepicker and datefns used

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
+import Appointment from './Pages/Appointment/Appointment';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto px-20'>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
       </Routes>
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -11,7 +11,7 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-130px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 p-2'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appointment</h2>
                 <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptatibus hic ab minima? Obcaecati sit nulla reprehenderit, sed quas qui, aliquid quis optio non molestiae exercitationem voluptas praesentium soluta labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit accusamus iure molestias illum asperiores officiis in similique at nesciunt animi, perspiciatis temporibus? Quaerat repellendus, similique obcaecati maxime alias necessitatibus natus.</p>
