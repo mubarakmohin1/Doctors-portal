f2fa8444ae5ebfa55faff725b7a173725ec476c5
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-13 11:37:58 +0600

    loading component shared and error hadnling in sighn in page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init'
 import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    if (loading || gLoading) {
+        return <Loading></Loading>
+      }
+       let signInError;
+       if(error || gError){
+           signInError = <p className='text-red-500'>{error?.message || gError?.message}</p>
+       }
+
+    const onSubmit = data => {
+        console.log(data);
+        signInWithEmailAndPassword(data.email,data.password)
+    };
+
     return (
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
@@ -66,6 +86,7 @@ const Login = () => {
 
                             </label>
                         </div>
+                        {signInError}
                         <input value='Login' className='btn btn-secondary w-full max-w-xs' type="submit" />
                     </form>
 
