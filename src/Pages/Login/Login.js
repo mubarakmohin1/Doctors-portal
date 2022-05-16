7d476a723ef2707354e1a9a5c1934ac6dd5c360d
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-16 16:45:49 +0600

    jwt token use

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,11 +4,12 @@ import auth from '../../firebase.init'
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../Hooks/useToken';
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const navigate = useNavigate();
-
+    
     const [
         signInWithEmailAndPassword,
         user,
@@ -16,14 +17,15 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
     
+    const [token] = useToken(user || gUser);
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
    useEffect(()=>{
-    if(user || gUser){
+    if(token){
         navigate(from, { replace: true });
     }
-   },[user,gUser,from,navigate])
+   },[ token,from,navigate])
 
     if (loading || gLoading) {
         return <Loading></Loading>
